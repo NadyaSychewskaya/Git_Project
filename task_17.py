f25504ffa198b96dd41e5db435e7a91f857e6c0b
NadyaSychewskaya <sychewskaya.nadya@gmail.com>
2022-06-19 16:34:06 +0300

    function first, then while cycle

--- task_17.py
+++ task_17.py
@@ -4,31 +4,31 @@
 # Ноль использовать в качестве завершения программы (сделать как отдельную операцию).
 
 
-while True:
+def divide():
+    return a / b
 
+def multiply():
+    return a * b
+
+def plus():
+    return a + b
+
+def minus():
+    return a - b
+
+while True:
     try:
         a = float(input('Введите 1-ое число'))
         b = float(input('Введите 2-ое число'))
         action = input('Введите операцию: + - * /')
 
         if action == '/':
-            def divide():
-                return a / b
             print(divide())
-
         elif action == '*':
-            def multiply():
-                return a * b
             print(multiply())
-
         elif action == '+':
-            def plus():
-                return a + b
             print(plus())
-
         elif action == '-':
-            def minus():
-                return a - b
             print(minus())
 
     except ValueError:
